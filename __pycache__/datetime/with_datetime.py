939756ba77c6519bccff15aa5abbee11d3cdbeea
Premsai Varma <pchekuri@gitam.in>
2022-11-22 18:44:16 +0530

    added datetime

---
+++ __pycache__/datetime/with_datetime.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+date_time = datetime(2022,11,23,12,0,0)
+print(date_time)
+print(date_time.year)
+print(date_time.month)
+print(date_time.hour)
+print(date_time.minute)
+
+# current date and time
+
+d_t = datetime.now()
+print(d_t)
+
+date_time = datetime(2022,11,23)
+print(date_time)
+
+
+
+
+
